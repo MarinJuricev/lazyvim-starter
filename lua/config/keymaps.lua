7415188eb89348df52e7e80c9fd5b7eb074a85cf
marinjuricev <mjuric00@fesb.hr>
2026-02-05 10:16:21 +0100

    chore: Update the keymap to be more inline with my flow

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,9 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del("n", "<leader>ff")
+
+-- Set <leader>sf to find files using snacks
+vim.keymap.set("n", "<leader>sf", function()
+  require("snacks").picker.files()
+end, { desc = "Find Files (Root Dir)" })
